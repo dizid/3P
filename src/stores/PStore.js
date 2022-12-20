e2dbe00701ffa05f8bcc3911115777f7b6056d9b
Marc <marc@dizid.com>
2022-12-20 17:13:49 +0100

    v0.1

--- src/stores/PStore.js
+++ src/stores/PStore.js
@@ -2,14 +2,30 @@ import { defineStore } from 'pinia'
 
 export const usePStore = defineStore({
   id: 'PStore',
-  state: () => ({  // Default values - or not?
-    baselinepoen: '',
-    baselinepret: '',
-    baselineprestige: '',
+  state: () => ({
+    baselinepoen: '50',
+    baselinepret: '50',
+    baselineprestige: '50',
     project: '',
-    projectpoen: '',
-    projectpret: '',
-    projectprestige: '',
+    projectpoen: '50',
+    projectpret: '50',
+    projectprestige: '50',
 
   }),
+
+  getters: {
+    endScore: (state) =>
+      (state.baselinepoen * state.projectpoen) +
+      (state.baselinepret * state.projectpret) +
+      (state.baselineprestige * state.projectprestige),
+
+    // Above getter WORKS but when i add 2nd getter (below) i get: endScore: undefined error
+
+    /*     endAdvice() {
+          if (endScore > 6000)
+            return "wel"
+          // else return "niet"
+        }, */
+  },
+
 })
--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,21 +1,24 @@
 <template>
     <div>
    <div class="bg-green-300 border-green-600 border-b p-4 m-4 rounded text-3xl">Op basis van jouw levensvisie en de waarden voor 
-    dit project, is het advies om dit wel/niet te gaan doen. </div>
+   project "{{ store.project }}", is het advies om dit <b>{{ Advice }}</b> te gaan doen. </div>
     
     <div>
-      Resultaat
-    </div>
+      <!-- TEST   score : {{ store.endScore }}<br> -->
+          </div>
     </div>
     </template>
     
     <script setup>
-// Imports
 import { usePStore } from '@/stores/PStore'
-// Connection with the Pinia store
-const answerStore = usePStore()
+import { computed } from 'vue'
+const store = usePStore()
 
-
+const Advice = computed (() => {
+    if (store.endScore > 6000)
+           { return "wel" }
+          else { return "niet"} 
+})
     </script>
     
   
